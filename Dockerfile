fa0a8c086a2196e0fd7eaab4daaaeccbef726dd7
Steve Elliott <steve@tegud.net>
2017-05-23 06:50:06 +0100

    eugh

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM tegud/docker-ghost-mysql-aws:latest
 COPY theme/ /usr/src/ghost/content/themes/tegud
 COPY config.js /config-example.js
 
+WORKDIR /usr/src/ghost/content/storage/s3
+
+RUN npm install aws-sdk
+
+WORKDIR /usr/src/ghost/
+
 ENTRYPOINT ["/entrypoint.sh"]
 
 CMD ["npm", "start", "--production"]
--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 var path = require('path'),
     config;
 
@@ -14,27 +13,36 @@ config = {
                 service: 'Mailgun',
                 auth: {
                     user: process.env.EmailUserName, // mailgun username
-                    pass: process.env.EmailPassword  // mailgun password
+                    pass: process.env.EmailPassword // mailgun password
                 }
             }
         },
         database: {
             client: 'mysql',
             connection: {
-                host     : 'mysql',
-                user     : 'GHOST_USER',
-                password : 'GHOST_PASSWORD',
-                database : 'GHOST_DB',
-                charset  : 'utf8'
+                host: 'mysql',
+                user: 'GHOST_USER',
+                password: 'GHOST_PASSWORD',
+                database: 'GHOST_DB',
+                charset: 'utf8'
+            }
+        },
+        storage: {
+            active: 's3',
+            s3: {
+                accessKeyId: process.env.AWS_KEY,
+                bucket: process.env.AWS_BUCKET,
+                region: process.env.AWS_REGION,
+                secretAccessKey: process.env.AWS_SECRET
             }
         },
         server: {
             host: '0.0.0.0',
             port: '2368'
         },
-      	paths: {
-      	    contentPath: path.join(__dirname, '/content/')
-      	}
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
     }
 };
 
