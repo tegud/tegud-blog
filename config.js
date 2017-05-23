d460840890a99b34ab98d6043b75b1e64996cb3f
Steve Elliott <steve@tegud.net>
2017-05-23 07:16:40 +0100

    and again

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+
 var path = require('path'),
     config;
 
@@ -13,36 +14,27 @@ config = {
                 service: 'Mailgun',
                 auth: {
                     user: process.env.EmailUserName, // mailgun username
-                    pass: process.env.EmailPassword // mailgun password
+                    pass: process.env.EmailPassword  // mailgun password
                 }
             }
         },
         database: {
             client: 'mysql',
             connection: {
-                host: 'mysql',
-                user: 'GHOST_USER',
-                password: 'GHOST_PASSWORD',
-                database: 'GHOST_DB',
-                charset: 'utf8'
-            }
-        },
-        storage: {
-            active: 's3',
-            s3: {
-                accessKeyId: process.env.AWS_KEY,
-                bucket: process.env.AWS_BUCKET,
-                region: process.env.AWS_REGION,
-                secretAccessKey: process.env.AWS_SECRET
+                host     : 'mysql',
+                user     : 'GHOST_USER',
+                password : 'GHOST_PASSWORD',
+                database : 'GHOST_DB',
+                charset  : 'utf8'
             }
         },
         server: {
             host: '0.0.0.0',
             port: '2368'
         },
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
+      	paths: {
+      	    contentPath: path.join(__dirname, '/content/')
+      	}
     }
 };
 
